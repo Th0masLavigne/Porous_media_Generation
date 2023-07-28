db7a43961439245bdd8c6443620b4610b392f7b3
Thomas Lavigne <91893320+Th0masLavigne@users.noreply.github.com>
2023-07-28 14:13:15 +0200

    Add files via upload

---
+++ Cylinder/Work_in_progress/28_07_fr_stl.py
@@ -0,0 +1,30 @@
+# Extract Vertices and Edges from .tess file Created using neper.
+# Support : https://neper.info/
+# https://pymesh.readthedocs.io/en/latest/
+# Done in meters
+# 
+from custom_stl2voxel import *
+import numpy
+import os
+import stltovoxel
+# 
+# 
+if __name__ == "__main__"  :
+		# Define the parameters of the problem:
+		number_of_seeds =5
+		# Define the outputs file names for the merged mesh, the boolean (CSG) mesh and the cleaned one
+		output_filename_perm = str(number_of_seeds)+'_seed_permeabilitty.stl'
+		#
+		#resolution_order = 7
+		res_ex = [80,80,160]
+		fold_name = 'n'+str(number_of_seeds)+'seed_'+ str(res_ex[0])+'x'+ str(res_ex[1])+'x'+ str(res_ex[2])+'_vx_2'
+		try: 
+			os.mkdir(fold_name)
+		except:
+			pass
+		#UL_convert_files([output_filename_perm],fold_name+'/xp', fold_name+'/chip', fold_name+'/meta', number_of_seeds, i_res=resolution_order, parallel= False)
+		Diameter = 24e-3
+		chamber_height = 10e-3
+		#UL_convert_files_cyl([output_filename_perm],fold_name+'/xp', fold_name+'/chip', fold_name+'/meta', Diameter, 0.9*Diameter, number_of_seeds, i_res=resolution_order, parallel= False)
+		# UL_convert_files_cyl_2([output_filename_perm],fold_name+'/xp', fold_name+'/chip', fold_name+'/meta', Diameter, 0.9*Diameter, number_of_seeds, resolution_expected=res_ex, parallel=False)
+		UL_convert_files_cyl_3([output_filename_perm],fold_name+'/xp', fold_name+'/chip', fold_name+'/meta', Diameter, 0.9*Diameter, number_of_seeds, chamber_height, expected_inlet_vx=6, voxel_size__=1e-4, parallel=False)
